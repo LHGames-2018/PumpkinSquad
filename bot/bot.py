a162e9fefdfb181afc0be3a34976e00427300199
Lillian <lillian.chiu@mail.mcgill.ca>
2018-10-06 14:53:17 -0400

    moving tiles
    moving tiles

--- bot/bot.py
+++ bot/bot.py
@@ -20,6 +20,8 @@ class Bot:
             :param visiblePlayers:  The list of visible players.
         """
 
+        # Write your bot here. Use functions from aiHelper to instantiate your actions.
+
         # Create map array
         map_vision = []
         for y in range(gameMap.yMin,gameMap.yMax):
@@ -27,13 +29,40 @@ class Bot:
             for x in range(gameMap.xMin, gameMap.xMax):
                 map_vision[y - gameMap.yMin].append(gameMap.getTileAt(Point(x,y)).value)
 
+        # initialize direction
+        # player is in constant motion
+
+        # MOVE
+
+        # while the ADJACENT TILE TO THE RIGHT is empty, move 1
+        while self.x(TileContent) == 0:
+            
+            # coordinates in (x,y)
+            # x increasing left to right
+            # y increasing top to bottom
+            create_move_action(1,0)
+
+
+
+        # ATTACK
+
+        if TileContent == 6:
+            create_attack_action(1,0)
+
+        # COLLECT
+
+        # STEAL
+
+        # HEAL
+
+        # PURCHASE
+
         # Show map
         # for x in map_vision:
         #     for y in x:
         #         print(y,end="")
         #     print()
 
-        # Write your bot here. Use functions from aiHelper to instantiate your actions.
         return create_move_action(Point(1, 0))
 
     def after_turn(self):
